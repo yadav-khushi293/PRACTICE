1fde10093265498f942d35ae237d99891c21d9de
yadav-khushi293 <yadavkhushi293@gmail.com>
2026-04-16 15:59:42 +0530

    code

--- Prectice/index.js
+++ Prectice/index.js
@@ -1,37 +1,73 @@
-const api=`https://fakestoreapi.com/products`;
+const api = "https://fakestoreapi.com/products";
 
-const ApiCall=()=>{
-    fetch(api)
-    .then((res)=>res.json())
-    .then((res)=>appendsFunc(res))
-    .catch((err)=>console.log(err))
+fetch(api)
+  .then((res) => res.json())
+  .then((data) => showData(data));
+
+function showData(items) {
+  let container = document.getElementById("data");
+
+  items.forEach((item) => {
+    container.innerHTML += `
+      <div class="col-12 col-md-6 col-lg-3 mb-4">
+
+        <div class="card h-100 shadow">
+
+          <img src="${item.image}" class="card-img-top" style="height:200px; object-fit:contain;">
+          
+          <div class="card-body">
+            <h6 class="card-title">${item.title}</h6>
+            <p class="text-success fw-bold">₹ ${item.price}</p>
+            <button class="btn btn-primary w-100">Buy Now</button>
+          </div>
+          
+        </div>
+
+      </div>
+
+
+    `;
+  });
 }
-ApiCall()
 
-const appendsFunc =(data)=>{
+// card → pura card container
+// card-body → card ke andar ka content area
+// card-title → heading
+// card-text → description
 
-    let dataShow = document.getElementById('info');
-     
-    data.forEach(el => {
-         
-        let maindiv = document.createElement("div");
-        let title = document .createElement('p');
-        let price = document.createElement('p');
-        let description = document.createElement('p');
-        let img = document .createElement('img');
-        
+// const api=`https://fakestoreapi.com/products`;
 
-        maindiv.class="";
+// const ApiCall=()=>{
+//     fetch(api)
+//     .then((res)=>res.json())
+//     .then((res)=>appendsFunc(res))
+//     .catch((err)=>console.log(err))
+// }
+// ApiCall()
 
-        title.classList="title";
+// const appendsFunc =(data)=>{
 
-        title.innerText=el.title;
-        price.innerText=el.price;
-        description.innerText=el.description;
-        img.src=el.image;
+//     let dataShow = document.getElementById('info');
 
-       maindiv.append(img,title,price,description)
-       dataShow .append(maindiv)
-    });
+//     data.forEach(el => {
 
-}
+//         let maindiv = document.createElement("div");
+//         let title = document .createElement('p');
+//         let price = document.createElement('p');
+//         let description = document.createElement('p');
+//         let img = document .createElement('img');
+
+//         maindiv.class="";
+
+//         title.classList="title";
+
+//         title.innerText=el.title;
+//         price.innerText=el.price;
+//         description.innerText=el.description;
+//         img.src=el.image;
+
+//        maindiv.append(img,title,price,description)
+//        dataShow .append(maindiv)
+//     });
+
+// }
